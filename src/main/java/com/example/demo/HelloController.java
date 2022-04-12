64c2c941ab86e68920429c291f6235d0078e7a02
kaczmarski <eukaczmarski@gmail.com>
2022-04-12 08:31:06 +0300

    hw2.4

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.demo;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.security.Key;
+
+import static javafx.scene.input.KeyCode.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    TextArea textArea;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Button button;
+
+    @FXML
+    TextField textField;
+
+
+    public void sendMessage(){
+        textArea.appendText(textField.getText()+ "\n");
+            textField.clear();
+            textField.requestFocus();
+    }
+    @FXML
+    protected void keySendMessage(KeyEvent kEnter){
+        if (kEnter.getCode() == KeyCode.ENTER) {
+            sendMessage();
+        }
     }
 }
